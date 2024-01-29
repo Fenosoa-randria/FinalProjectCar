b93d56426a53463a66b1044e2fff2d1694163991
fenosoarandria <119055925+fenosoarandria@users.noreply.github.com>
2024-01-29 19:53:09 +0300

    first commit

---
+++ VenteVoiture/src/main/java/monpackage/ventevoiture/model/CorsConfig.java
@@ -0,0 +1,21 @@
+package monpackage.ventevoiture.model;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // Remplacez par l'URL de votre application React
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
