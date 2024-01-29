1bb518d535509ef68a1bb02c28472a944eaa0885
fenosoarandria <119055925+fenosoarandria@users.noreply.github.com>
2024-01-29 19:52:33 +0300

    g

--- VenteVoiture/src/main/java/monpackage/ventevoiture/Repository/MieuxVisiterRepository.java
+++
@@ -1,8 +0,0 @@
-package monpackage.ventevoiture.Repository;
-
-import monpackage.ventevoiture.model.MieuxVisiter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MieuxVisiterRepository extends JpaRepository<MieuxVisiter,Integer> {
-
-}
--- VenteVoiture/src/main/java/monpackage/ventevoiture/model/AdminStatistiqueId.java
+++
@@ -1,9 +0,0 @@
-package monpackage.ventevoiture.model;
-
-import java.io.Serializable;
-
-public class AdminStatistiqueId implements Serializable {
-    private int nombre;
-    private String marque;
-    private String annee;
-}
